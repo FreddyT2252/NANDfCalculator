55be92f11cae0cd3d9b072cafe71756f2c09d0b6
FreddyT2252 <79196216+FreddyT2252@users.noreply.github.com>
2021-04-16 09:21:26 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CalculateDemo
+{
+    class Program
+    {
+
+        static Dictionary<string, string> cfg = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {
+            Load();
+            while(true)
+            {
+                Console.Write(" masukkan :");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Please enter operand 2:");
+                int y = int.Parse(Console.ReadLine());
+                Console.Write("Please enter the operator:");
+                string op = Console.ReadLine();
+                Operator opr = OperatorFactory.Create(cfg[op]);
+                opr.1 = x;
+                opr.2 = y;
+                int r = opr.Calc();
+                Console.WriteLine($"{x}{op}{y}={r}");
+            }
+        }
+        /// <summary>
+        /// Initialization assignment
+        /// </summary>
+        static void Load()
+        {
+            cfg.Add("+", "CalculateDemo.AddOperator");
+            cfg.Add("-","CalculateDemo.SubOperator");
+            cfg.Add("*", "CalculateDemo.MulOperator");
+            cfg.Add("/", "CalculateDemo.DivOperator");
+        }
+    }
+}
